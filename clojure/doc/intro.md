288ad549c65d9d3eaf67d129e6aca76b97a6fcf3
Austin Benesh <austin@austinbenesh.com>
2019-12-28 22:10:09 -0700

    Initial commit

---
+++ clojure/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bipartite-graph
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure/src/bipartite_graph/core.clj
@@ -0,0 +1,63 @@
+(ns bipartite-graph.core
+  (:refer-clojure))
+
+(def example-graph
+  {:nodes [
+   {:id 1 :relationships [2 6]}
+   {:id 2 :relationships [1 3]}
+   {:id 3 :relationships [2 4]}
+   {:id 4 :relationships [3 5]}
+   {:id 5 :relationships [4 6]}
+   {:id 6 :relationships [5 1]}
+    ]
+  }
+)
+
+(def example-graph-2
+  {:nodes [
+   {:id 1 :relationships [2 6 9]}
+   {:id 2 :relationships [1 3]}
+   {:id 3 :relationships [2 4]}
+   {:id 4 :relationships [3 5]}
+   {:id 5 :relationships [4 6]}
+   {:id 6 :relationships [5 1 9]}
+   {:id 7 :relationships [6 8]}
+   {:id 8 :relationships [5 7]}
+   {:id 9 :relationships [1 6]}
+    ]
+  }
+)
+
+(defn bipartite?
+  "Determines whether a given graph is bipartite."
+  [graph]
+  (let [nodes (:nodes graph)]
+    (loop [cur-node (first nodes)
+           rest-of-nodes (rest nodes)
+           cur-key (keyword 'a)
+           maps {:a [] :b []}]
+      (if (some #(.contains (get maps cur-key) %) (:relationships cur-node))
+        false
+        (if (zero? (count rest-of-nodes))
+          true
+          (recur
+           (first rest-of-nodes)
+           (rest rest-of-nodes)
+           (if (= (keyword 'a) cur-key)
+             (keyword 'b)
+             (keyword 'a)
+             )
+           (merge-with into maps {cur-key [(:id cur-node)]})))))))
+
+
+(defn -main
+  []
+  (print example-graph)
+  (print "\n\n")
+  (print (bipartite? example-graph))
+  (print "\n\n")
+  (print example-graph-2)
+  (print "\n\n")
+  (print (bipartite? example-graph-2))
+  (print "\n\n"))
+
