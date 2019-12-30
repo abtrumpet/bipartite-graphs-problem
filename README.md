80f5d6b1089f3061880a68420a5e776d6ee5909a
Austin Benesh <austin@austinbenesh.com>
2019-12-30 16:29:06 -0700

    Added Javascript implementation

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 A number of implementations of the bipartite graphs problem.
 
 ## Clojure
+Will need `leiningen` build tool.
 To run, clone the project `cd` into the "clojure" dir and type:
 ```
 lein run
 ```
+
+## JavaScript
+Will need `nodejs` and `npm` installed.
+To run, clone the project `cd` into the "javascript" dir and type:
+```
+npm start
+```
+
---
+++ javascript/src/index.js
@@ -0,0 +1,52 @@
+const exampleGraph = {
+  nodes: [
+    {id: 1, relationships: [2, 6]},
+    {id: 2, relationships: [1, 3]},
+    {id: 3, relationships: [2, 4]},
+    {id: 4, relationships: [3, 5]},
+    {id: 5, relationships: [4, 6]},
+    {id: 6, relationships: [5, 1]}
+  ]
+};
+
+const exampleGraph2 = {
+  nodes: [
+    {id: 1, relationships: [2, 6, 9]},
+    {id: 2, relationships: [1, 3]},
+    {id: 3, relationships: [2, 4]},
+    {id: 4, relationships: [3, 5]},
+    {id: 5, relationships: [4, 6]},
+    {id: 6, relationships: [5, 1, 9]},
+    {id: 7, relationships: [6, 8]},
+    {id: 8, relationships: [5, 7]},
+    {id: 9, relationships: [1, 6]}
+  ]
+};
+
+function isBipartite(graph) {
+  const { nodes } = graph;
+  
+  function _isBipartite(curNode = {}, restOfNodes = [], curKey = "a", maps = {a: [], b: []}) {
+    if (restOfNodes.length == 0)
+      return true;
+
+    if(curNode.relationships.some(x =>
+      maps[curKey] && maps[curKey].includes(x)
+    )) return false;
+
+    return _isBipartite(
+      restOfNodes[0], 
+      restOfNodes.splice(1), 
+      curKey == "a" ? "b" : "a",
+      {...maps, [curKey]: [...maps[curKey], curNode.id]}
+    );
+  }
+
+  return _isBipartite(nodes[0], nodes.splice(1));
+}
+
+console.log(exampleGraph + "\n");
+console.log(isBipartite(exampleGraph) + "\n");
+console.log(exampleGraph2 + "\n");
+console.log(isBipartite(exampleGraph2) + "\n");
+
